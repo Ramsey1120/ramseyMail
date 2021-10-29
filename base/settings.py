a3d7c1f423f47119cfc6b6ae0d4a3ed8fd6daafc
Ramsey Ediku CS2019 <ramsey.ediku.2020@uni.strath.ac.uk>
2021-10-29 21:59:29 +0100

    fixed settings.py file

--- base/settings.py
+++ base/settings.py
@@ -17,12 +17,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '05$4$3aew(8ywondz$g!k4m779pbvn9)euj0zp7-ae*x@4pxr+'
+SECRET_KEY = '05$4$3aew(8ywondz$g!k4m779pbvn9)euj0zp7-ae*x@4pxr+' # generate new key for production
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://ramsey-mail.herokuapp.com'] # added this for production
 
 
 # Application definition
