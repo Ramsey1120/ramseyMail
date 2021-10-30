13824c58c678cff13329a409b3add7928024b31e
Ramsey Ediku CS2019 <ramsey.ediku.2020@uni.strath.ac.uk>
2021-10-30 08:57:55 +0100

    tweaked settings.py for developemtn

--- base/settings.py
+++ base/settings.py
@@ -19,14 +19,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # generate new key for production
-#SECRET_KEY = '05$4$3aew(8ywondz$g!k4m779pbvn9)euj0zp7-ae*x@4pxr+' 
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = '05$4$3aew(8ywondz$g!k4m779pbvn9)euj0zp7-ae*x@4pxr+' 
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
+DEBUG = True
 
-ALLOWED_HOSTS = ['ramsey-mail.herokuapp.com'] # added this for production
+ALLOWED_HOSTS = [''] # added this for production
 
 
 # Application definition
@@ -122,4 +121,3 @@ USE_TZ = False
 STATIC_ROOT = os.path.join(BASE_DIR,"staticfiles") # remember to add before production  
 STATIC_URL = '/static/'
 
-django_heroku.settings(locals())
